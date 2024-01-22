d33a837e6f4e11dced4d44b642e5f5c3d13a6138
DevMirza <pzhafeez@gmail.com>
2024-01-22 11:17:11 +0000

    update

---
+++ src/components/custom-avatar.tsx
@@ -0,0 +1,24 @@
+import { Avatar as AntdAvatar, AvatarProps } from "antd";
+
+type Props = AvatarProps & {
+  name: string;
+};
+
+const CustomAvatar = ({ name, style, ...rest }: Props) => {
+  return (
+    <AntdAvatar
+      alt={"Zaid Hafeez"}
+      size="small"
+      style={{
+        backgroundColor: "#87d868",
+        display: "flex",
+        alignItems: "center",
+        border: "none",
+      }}
+    >
+      {name}
+    </AntdAvatar>
+  );
+};
+
+export default CustomAvatar;
